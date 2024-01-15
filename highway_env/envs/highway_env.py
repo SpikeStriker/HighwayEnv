d8a34b416b41c7b7a30ded62e889763fc784fdc9
Uday Jain <uday9k@gmail.com>
2024-01-15 23:39:42 +0100

    adding lane change reward normalisation seperately

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -108,9 +108,8 @@ class HighwayEnv(AbstractEnv):
                 [
                     self.config["collision_reward"],
                     self.config["high_speed_reward"] 
-                    + self.config["right_lane_reward"]
-                    # + self.config["lane_change_reward"]
-                    ,
+                    + self.config["right_lane_reward"],
+                    self.config["lane_change_reward"],
                 ],
                 [0, 1],
             )
